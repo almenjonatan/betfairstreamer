7e633ee33a369416863d860f812e6d8d3d5831b7
Jonatan Almen <almen.jonatan@gmail.com>
2020-03-12 18:35:35 +0100

    Add tox, add typing support for being used as 3rd party library with typing

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="betfairstreamer",  # Replace with your own username
+    name="betfairstreamer",
     version="0.2.2",
     author="Jonatan Almen",
+    zip_safe=False,
     author_email="almen.jonatan@gmail.com",
     description="Server for Exchange Stream API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/almenjonatan/betfairstreamer.git",
-    packages=setuptools.find_packages(),
+    packages=["betfairstreamer", "betfairstreamer.betfair", "betfairstreamer.cache"],
+    package_data={"betfairstreamer": ["py.typed"]},
     install_requires=[
         "betfairlightweight",
         "orjson",
@@ -22,12 +24,8 @@ setuptools.setup(
         "attrs",
         "pytz",
     ],
-    entry_points={
-        "console_scripts": ["betfairstreamer = betfairstreamer.server:start",],
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-    ],
+    include_package_data=True,
+    entry_points={"console_scripts": ["betfairstreamer = betfairstreamer.server:start"]},
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License"],
     python_requires=">=3.8.0",
 )
